1a5e23e30aa4f3bbb7f599be6f58c0c9a460bab3
foreverealize <60579848+FOREVEREALIZE@users.noreply.github.com>
2024-12-13 20:01:17 +0100

    Updates

--- assets/index-spRmRKcm.js
+++ assets/index-spRmRKcm.js
@@ -1,6 +1,6 @@
 (function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&r(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();document.querySelector("#app").innerHTML=`
   <div class="w-screen h-screen flex items-center justify-center bg-black overflow-hidden">
-    <button id="holy-button" class="bg-orange-100 py-4 px-20 text-2xl inter-900 rounded-xl drop-shadow-glow">
+    <button id="holy-button" class="bg-orange-100 py-4 px-20 text-2xl inter-900 rounded-xl drop-shadow-glow select-none">
         Holy.
     </button>
     <audio src="sound.mp3" id="holy-audio"></audio>
